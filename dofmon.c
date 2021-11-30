2b7be15f5dc55b9e8a246d4a4c3946037bac4358
harshilkumar <95012680+harshilkumar@users.noreply.github.com>
2021-11-30 12:21:47 +0530

    Add files via upload

---
+++ dofmon.c
@@ -0,0 +1,55 @@
+//Days of given month
+#include<stdio.h>
+void main()
+
+{ 
+	int a;
+	
+	printf("Enter a Month=");
+	scanf("%d",&a);
+	
+	
+
+	switch(a)
+	{
+		case 1:
+		printf("In This Month has 31 Days ");
+		break;
+		case 2:
+		printf("In This Month has 28 or 29 Days ");
+		break;
+		case 3:
+		printf("In This Month has 31 Days ");
+		break;
+		case 4:
+		printf("In This Month has 30 Days ");
+		break;
+		case 5:
+		printf("In This Month has 31 Days ");
+		break;
+		case 6:
+		printf("In This Month has 30 Days ");
+		break;
+		case 7:
+		printf("In This Month has 31 Days ");
+		break;
+		case 8:
+		printf("In This Month has 31 Days ");
+		break;
+		case 9:
+		printf("In This Month has 30 Days ");
+		break;
+		case 10:
+		printf("In This Month has 31 Days ");
+		break;
+		case 11:
+		printf("In This Month has 30 Days ");
+		break;
+		case 12:
+		printf("In This Month has 31 Days ");
+		break;
+		
+	}
+
+}
+  
